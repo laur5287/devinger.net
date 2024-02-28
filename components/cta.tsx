850760b17083c23326a18360c6b66544cc7f937a
Lr.S <78878365+laur5287@users.noreply.github.com>
2024-02-28 08:52:32 +0200

    aceternity component moving-borders added

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Link from "next/link"
 import ContactForm from "./ContactForm"
 import { DrawerDialog } from "./Drawer"
@@ -6,22 +7,26 @@ import Popover from "./Popover"
 import { Button } from "./ui/button"
 import { buttonVariants } from "./ui/button"
 import { cn } from "@/lib/utils"
+import { ButtonAceternity } from "@/components/aceternity/moving_border";
 
 const CallToAction = () => {
 	return (
 		<div id='cta' className="flex gap-2">
-			{/* <div className="relative flex items-center justify-center gap-5 sm:m-auto"> */}
-			{/* <Button size='xl' variant='outline'>Projects</Button> */}
-			{/* <Popover content={<ContactForm />} >
-					<Button className='' size='xl'>Contact</Button>
 
-				</Popover> */}
 			<Link className={cn('font-bold', buttonVariants({ variant: "default", size: 'xl' }))
 			} href='#projects_section' >Projects</Link>
-			<DrawerDialog content={<ContactForm />}>
 
-				<Button size='xl'>Contact</Button>
-			</DrawerDialog >
+
+			<ButtonAceternity
+				borderRadius="1.75rem"
+				className=" border-neutral-200 dark:border-slate-800"
+			>
+				<DrawerDialog content={<ContactForm />}>
+
+
+					<Button size='xl'>Contact</Button>
+				</DrawerDialog >
+			</ButtonAceternity>
 		</div>
 	)
 }
